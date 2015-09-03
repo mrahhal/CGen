0648006ec72a4c10cdd0031146ad044973236861
Mohammad Rahhal <m.r992@hotmail.com>
2015-09-03 21:58:11 +0300

    Initial commit

---
+++ src/CGen/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CGen
+{
+	public class Program
+	{
+		private static void Main(string[] args)
+		{
+		}
+	}
+}
---
+++ src/CGen/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CGen")]
+[assembly: AssemblyDescription("Generates different kinds of cryptographic keys")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("MRH")]
+[assembly: AssemblyProduct("CGen")]
+[assembly: AssemblyCopyright("Copyright © 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("d968d46d-00d6-4626-8a5a-fc576033ce5a")]
+
+[assembly: AssemblyVersion("1.0.0.*")]
