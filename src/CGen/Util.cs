0b4a9ee57ce5907c51505d9d23562fbf2fc9c278
Mohammad Rahhal <m.r992@hotmail.com>
2015-09-04 11:52:16 +0300

    + Util

---
+++ src/CGen/Util.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace CGen
+{
+	public static class Util
+	{
+		private static string[] s_hexMap = Enumerable.Range(0, 256).Select(v => v.ToString("X2")).ToArray();
+
+		public static string ToHex(byte[] array)
+		{
+			var s = new StringBuilder(array.Length * 2);
+			foreach (var v in array)
+				s.Append(s_hexMap[v]);
+			return s.ToString();
+		}
+
+		public static byte[] RandomBytes(int length)
+		{
+			var crypto = new RNGCryptoServiceProvider();
+			var bytes = new byte[length];
+			crypto.GetBytes(bytes);
+			return bytes;
+		}
+	}
+}
