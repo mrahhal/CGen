cbde0bb806775058fec8a80c19c1a8eb21c87daf
Mohammad Rahhal <m.r992@hotmail.com>
2015-10-27 16:47:04 +0200

    Remove unneeded namespaces

--- src/CGen/Program.cs
+++ src/CGen/Program.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using CGen.CommandLine;
+﻿using CGen.CommandLine;
 using Konsola;
 
 namespace CGen
@@ -14,6 +11,6 @@ namespace CGen
 			if (context == null)
 				return;
 			context.Command.ExecuteCommand();
-        }
+		}
 	}
 }
