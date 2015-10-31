d40d2fcf40fa4021866d399fc216b021fb0caffe
Mohammad Rahhal <m.r992@hotmail.com>
2015-10-31 18:45:17 +0200

    + GuidGeneratorCommand

---
+++ src/CGen/CommandLine/GuidGeneratorCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Konsola;
+using Konsola.Parser;
+
+namespace CGen.CommandLine
+{
+	[Command("guid", Description = "generate a new guid")]
+	public class GuidGeneratorCommand : Command
+	{
+		public override string ExecuteCore()
+		{
+			return Guid.NewGuid().ToString();
+		}
+	}
+}
