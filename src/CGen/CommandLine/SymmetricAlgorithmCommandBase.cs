f89da67c1f69034fb02ede164a88b99457e1924d
Mohammad Rahhal <m.r992@hotmail.com>
2015-11-01 11:00:08 +0200

    Reuse encryption stuff in a base command

--- src/CGen/CommandLine/SymmetricAlgorithmCommandBase.cs
+++ src/CGen/CommandLine/SymmetricAlgorithmCommandBase.cs
@@ -4,52 +4,8 @@ using Konsola.Parser;
 
 namespace CGen.CommandLine
 {
-	public abstract class SymmetricAlgorithmCommandBase : Command
+	public abstract class SymmetricAlgorithmCommandBase : EncryptionAlgorithmCommandBase
 	{
-		[Parameter("data", Description = "the data", Position = 1)]
-		public string Data { get; set; }
-
-		[Parameter("input", Description = "the file's name or path that contains the data")]
-		public string InputFileName { get; set; }
-
-		[Parameter("key", Description = "the key to use in the algorithm", IsMandatory = true)]
-		public string Key { get; set; }
-
-		[Parameter("encrypt", Description = "encrypt the data")]
-		public bool IsEncrypt { get; set; }
-
-		[Parameter("decrypt", Description = "decrypt the data")]
-		public bool IsDecrypt { get; set; }
-
-		[OnParsed]
-		public virtual void OnParsed()
-		{
-			if ((IsEncrypt && IsDecrypt) || (!IsEncrypt && !IsDecrypt))
-			{
-				throw new CommandLineException("Specify only one option to do, either encrypt or decrypt.")
-				{
-					Kind = CommandLineExceptionKind.Message,
-				};
-			}
-
-			if (Data == null && InputFileName == null)
-			{
-				throw new CommandLineException("Either data or a file name should be provided.")
-				{
-					Kind = CommandLineExceptionKind.Message,
-				};
-			}
-
-			if (InputFileName != null && !File.Exists(InputFileName))
-			{
-				throw new CommandLineException(
-					string.Format("The file at \"{0}\" does not exist.", InputFileName))
-				{
-					Kind = CommandLineExceptionKind.Message,
-				};
-			}
-		}
-
 		public override string ExecuteCore()
 		{
 			var data =
